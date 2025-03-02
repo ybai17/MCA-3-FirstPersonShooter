6427598c9601423cd157681d77d2b9a5b839d9a1
Yilei Bai <marbink100@gmail.com>
2025-03-02 17:18:25 -0500

    added sound effect for 0 shield charges + call UI update

--- Assets/FPSGameAssets/Scripts/CastShield.cs
+++ Assets/FPSGameAssets/Scripts/CastShield.cs
@@ -2,11 +2,16 @@ using UnityEngine;
 
 public class CastShield : MonoBehaviour
 {
+    public int maxShieldCount;
+    int currentShieldCount;
+
+    public AudioClip noShieldSFX;
+
     public GameObject shield;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        currentShieldCount = maxShieldCount;
     }
 
     // Update is called once per frame
@@ -16,7 +21,13 @@ public class CastShield : MonoBehaviour
             return;
             
         if (Input.GetKeyDown(KeyCode.Q)) {
-            shield.SetActive(true);
+            if (currentShieldCount > 0) {
+                shield.SetActive(true);
+                currentShieldCount -= 1;
+                GameObject.FindGameObjectWithTag("UI").GetComponent<UIManager>().UpdateShieldCount(currentShieldCount);
+            } else {
+                AudioSource.PlayClipAtPoint(noShieldSFX, transform.position);
+            }
         }
     }
 }
